84354d3cc6d6b0ee38a9b524a2d62295318060fd
BryanRyczek <bryanryczek@gmail.com>
2015-12-03 14:22:53 -0500

    commit

--- FlowMoSwift/ViewController.swift
+++ FlowMoSwift/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print ("HI")
+        print ("Ho")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
