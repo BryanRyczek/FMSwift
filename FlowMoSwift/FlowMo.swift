4b3952538753c9cbba0540eb60af89593136ac78
ccarey221 <careyc@union.edu>
2015-12-08 10:32:01 -0500

    Orientation set, Controller and model files added

---
+++ FlowMoSwift/FlowMo.swift
@@ -0,0 +1,14 @@
+//
+//  FlowMo.swift
+//  FlowMoSwift
+//
+//  Created by Conor Carey on 12/8/15.
+//  Copyright © 2015 Bryan Ryczek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+import AVKit
+import CoreMedia
+
