4d4e1249822026dfd2524b5235ba141058928ddd
BryanRyczek <bryanryczek@gmail.com>
2015-12-11 12:23:23 -0500

    Commit of working image generator method

--- FlowMoSwift/FlowMoController.swift
+++ FlowMoSwift/FlowMoController.swift
@@ -51,96 +51,36 @@ class FlowMoController: UIViewController, AVCaptureFileOutputRecordingDelegate {
     func generateImageSequence(outputFileURL: NSURL) {
         let avURLAsset = AVURLAsset(URL: outputFileURL, options:nil)
         
+        
         let imageGenerator = AVAssetImageGenerator.init(asset: avURLAsset)
         imageGenerator.requestedTimeToleranceAfter=kCMTimeZero
         imageGenerator.requestedTimeToleranceBefore=kCMTimeZero
         
         var imageHashRate: [NSValue] = []
+        //NEED TO PLUG THESE VALUES INTO THE BELOW LOOP TO GENERATE imageHashRateArray ONCE FURTHER WORK DONE
         var loopDuration = avURLAsset.duration.value
         let timeValue = Float(CMTimeGetSeconds(avURLAsset.duration))
-        print (loopDuration)
         
-        for var t = 0; t < 1800; t + 20 {
-            var cmTime = CMTimeMake(Int64(t), avURLAsset.duration.timescale)
-            var timeValue = NSValue(CMTime: cmTime)
+        for var t = 0; t < 1800; t += 20 {
+            let cmTime = CMTimeMake(Int64(t), avURLAsset.duration.timescale)
+            let timeValue = NSValue(CMTime: cmTime)
             imageHashRate.append(timeValue)
-            
-            var imageArray: [UIImage]
-            if (!imageArray) {
-                imageArray =
+            print(imageHashRate)
             }
-            
-        }
         
-//        -(NSMutableArray *)generateImageSequence:(NSURL *)outputFileURL
-//        {
-//            
-//            NSURL *url = outputFileURL;
-//            AVURLAsset *urlAsset = [[AVURLAsset alloc] initWithURL:url options:[NSDictionary dictionaryWithObject:@"YES" forKey:AVURLAssetPreferPreciseDurationAndTimingKey]]; //Change Object to @"NO" to turn off precise access.
-//            
-//            AVAssetImageGenerator *imageGenerator = [AVAssetImageGenerator assetImageGeneratorWithAsset:urlAsset];
-//            imageGenerator.requestedTimeToleranceAfter=kCMTimeZero;
-//            imageGenerator.requestedTimeToleranceBefore=kCMTimeZero;
-//            
-//            NSMutableArray *thumbTimes = [[NSMutableArray alloc]init];
-//            NSInteger assetDuration = urlAsset.duration.value;
-//            
-//            if (assetDuration > recordingDuration) {
-//                assetDuration = recordingDuration;
-//            }
-//            
-//            
-//            for(int t=flowMoStart; t < assetDuration; t=t+20) {
-//                CMTime thumbTime = CMTimeMake(t, urlAsset.duration.timescale);
-//                NSValue *timeValue=[NSValue valueWithCMTime:thumbTime];
-//                [thumbTimes addObject:timeValue];
-//            }
-//            
-//            if (!_imageArray) {
-//                _imageArray = [[NSMutableArray alloc] initWithCapacity:thumbTimes.count];
-//            } else {
-//                [_imageArray removeAllObjects];
-//                
-//            }
-//            
-//            //RESET flowMoStart, recordingDuration
-//            flowMoStart = 0;
-//            recordingDuration = 3000;
-//            
-//            
-//            [imageGenerator generateCGImagesAsynchronouslyForTimes:thumbTimes
-//                completionHandler:^(CMTime requestedTime, CGImageRef image, CMTime actualTime,
-//                AVAssetImageGeneratorResult result, NSError *error)
-//                {
-//                if (result == AVAssetImageGeneratorSucceeded) {
-//                
-//                NSLog(@"SUCCESS!");
-//                if ( _cameraOrientation == 1) {
-//                
-//                [_imageArray addObject:[UIImage imageWithCGImage:image scale:1.0 orientation:UIImageOrientationRight]];
-//                
-//                } else if (_cameraOrientation == 2) {
-//                
-//                [_imageArray addObject:[UIImage imageWithCGImage:image scale:1.0 orientation:UIImageOrientationLeftMirrored]];
-//                
-//                }
-//                
-//                if (thumbTimes.count == self.imageArray.count) {
-//                
-//                [self present: outputFileURL];
-//                }
-//                }
-//                if (result == AVAssetImageGeneratorFailed) {
-//                NSLog(@"Failed with error: %@", [error localizedDescription]);
-//                }
-//                if (result == AVAssetImageGeneratorCancelled) {
-//                NSLog(@"Canceled");
-//                }
-//                
-//                }];
-//            
-//            return _imageArray;
-//            
-//        }
+        var flowMoImageArray: [UIImage] = []
+        imageGenerator.generateCGImagesAsynchronouslyForTimes(imageHashRate) { (requestedTime, image, actualTime, result, error) -> Void in
+            if (result == .Succeeded) {
+                flowMoImageArray.append(UIImage(CGImage: image!))
+                print("SUCCESS!")
+            }
+            if (result == .Failed) {
+                
+            }
+            if (result == .Cancelled) {
+                
+            }
+        }
     }
+        
 }
