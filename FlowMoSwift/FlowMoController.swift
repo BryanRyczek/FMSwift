46350ccc689c4a801a0169faf1426749e400ce6c
BryanRyczek <bryanryczek@gmail.com>
2015-12-15 14:32:11 -0500

    Added framerate definition parameters to videoDataCapture method

--- FlowMoSwift/FlowMoController.swift
+++ FlowMoSwift/FlowMoController.swift
@@ -18,8 +18,6 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
     // MARK: GLOBAL VARS
     // define capture session
     let captureSession = AVCaptureSession()
-    // define video output
-    var videoFileOutput : AVCaptureMovieFileOutput?
     // define video input devices
     var backFacingCamera:AVCaptureDevice?
     var frontFacingCamera:AVCaptureDevice?
@@ -30,7 +28,7 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
     var audioFileOutput : AVCaptureAudioDataOutput?
     // var to denote recording status
     var isRecording = false
-    
+   
     var torchState = 0
     
     let videoDataOutput = AVCaptureVideoDataOutput()
@@ -38,13 +36,15 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
     var sessionQueue:dispatch_queue_t = dispatch_queue_create("com.example.session_access_queue", DISPATCH_QUEUE_SERIAL)
     var flowMoImageArray: [UIImage] = []
     
+    //MARK: Relegated video variables
+    // define video output
+    var videoFileOutput : AVCaptureMovieFileOutput?
     
+    //MARK: METHODS
     //MARK: CAMERA METHODS
     func realtimeCam() {
         captureSession.sessionPreset = AVCaptureSessionPresetHigh
-        
         let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo) as! [AVCaptureDevice]
-        
         for device in devices {
             if device.position == AVCaptureDevicePosition.Back {
                 backFacingCamera = device
@@ -52,9 +52,7 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
                 frontFacingCamera = device
             }
         }
-        
         currentDevice = backFacingCamera
-        
         let captureDeviceInput:AVCaptureDeviceInput
         do {
             captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice)
@@ -62,7 +60,36 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
             print(error)
             return
         }
-        
+        do {
+            //AVCaptureDeviceFormat allows for setting of framerate
+            var finalFormat = AVCaptureDeviceFormat()
+            var maxFps: Double = 0
+            var maxFpsDesired: Double = 60
+            for vFormat in currentDevice!.formats {
+                var ranges = vFormat.videoSupportedFrameRateRanges as! [AVFrameRateRange]
+                let frameRates = ranges[0]
+                
+                if frameRates.maxFrameRate >= maxFps && frameRates.maxFrameRate <= maxFpsDesired {
+                    maxFps = frameRates.maxFrameRate
+                    finalFormat = vFormat as! AVCaptureDeviceFormat
+                }
+            }
+            if maxFps != 0 {
+            let timeValue = Int64(1800.0 / maxFps)
+            let timeScale: Int32 = 1800
+                do {
+                    try currentDevice!.lockForConfiguration()
+                } catch {
+                    print(error)
+                    return
+                }
+            currentDevice!.activeFormat = finalFormat
+            currentDevice!.activeVideoMinFrameDuration = CMTimeMake(timeValue, timeScale)
+            currentDevice!.activeVideoMaxFrameDuration = CMTimeMake(timeValue, timeScale)
+                print(currentDevice!.activeVideoMaxFrameDuration)
+                print(currentDevice!.activeVideoMinFrameDuration)
+            currentDevice!.unlockForConfiguration()
+        }
         captureSession.addInput(captureDeviceInput)
         
         videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey: Int(kCVPixelFormatType_32BGRA)]
@@ -71,20 +98,18 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
         if captureSession.canAddOutput(videoDataOutput){
             captureSession.addOutput(videoDataOutput)
         }
-        
         if self.captureSession.canAddOutput(self.stillCameraOutput) {
             self.captureSession.addOutput(self.stillCameraOutput)
         }
         
+        }
     }
     
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
             let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
             let image = CIImage(CVPixelBuffer: pixelBuffer!)
     }
-        
     
-    // TODO: Wire this method up
     func retainImageFromBuffer(sender: AnyObject) {
         
     dispatch_async(sessionQueue) { () -> Void in
@@ -109,7 +134,7 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
     
     if let image = UIImage(data: imageData) {
         self.flowMoImageArray.append(image)
-        print("SUCCESS!")
+        print(self.flowMoImageArray.count)
         }
     } else {
     NSLog("error while capturing still image: \(error)")
@@ -138,58 +163,25 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
             //Ask user to change permissions in settings
             break
         }
-            
         }
     
-    func loadCamera(){
-    //set camera to highest resolution device will support
-    captureSession.sessionPreset = AVCaptureSessionPresetHigh
-    // create array of available devices (front camera, back camera, microphone)
-    let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo) as! [AVCaptureDevice]
-        
-    for device in devices {
-        if device.position == AVCaptureDevicePosition.Back {
-            backFacingCamera = device
-        } else if device.position == AVCaptureDevicePosition.Front {
-            frontFacingCamera = device
+    func capture (sender: UILongPressGestureRecognizer) {  //cont
+        //if we are not currently recording
+        if (sender.state == UIGestureRecognizerState.Ended){
+            isRecording = false
+            //videoFileOutput?.stopRecording()
+            fireTorch(sender)
+            print ("stop recording")
         }
-    }
-        
-    currentDevice = backFacingCamera
-    
-    let captureDeviceInput:AVCaptureDeviceInput
-        do {
-            captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice)
-        } catch {
-            print(error)
-            return
-    }
-    
-    //create instance used to save data for movie file
-    videoFileOutput = AVCaptureMovieFileOutput()
-    videoFileOutput?.maxRecordedDuration
-        
-    //create instance used to save audio data
-        
-    let audioDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeAudio)
-        
-    let audioInput:AVCaptureDeviceInput
-        do {
-            audioInput = try AVCaptureDeviceInput(device:audioDevice)
-        } catch {
-            print(error)
-            return
-    }
-    
-    audioFileOutput = AVCaptureAudioDataOutput()
-        
-    //Assign the input and output devices to the capture session
-    //captureSession.addInput(captureDeviceInput)
-    captureSession.addInput(audioInput)
-    captureSession.addOutput(videoFileOutput)
-    captureSession.addOutput(audioFileOutput)
-        
-    }
+        else if (sender.state == UIGestureRecognizerState.Began){
+            isRecording = true
+            captureAnimationBar()
+            print ("start recording")
+            fireTorch(sender)
+            //let outputPath = NSTemporaryDirectory() + "output.mov"
+            //let outputFileURL = NSURL(fileURLWithPath: outputPath)
+            //videoFileOutput?.startRecordingToOutputFileURL(outputFileURL, recordingDelegate: self)
+        }}
     
     func toggleCamera (sender: AnyObject) {
         captureSession.beginConfiguration()
@@ -218,25 +210,6 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
         currentDevice = newDevice
         captureSession.commitConfiguration()
     }
-
-
-    func capture (sender: UILongPressGestureRecognizer) {  //cont
-    //if we are not currently recording
-        if (sender.state == UIGestureRecognizerState.Ended){
-            isRecording = false
-            //videoFileOutput?.stopRecording()
-            fireTorch(sender)
-            print ("stop recording")
-            }
-        else if (sender.state == UIGestureRecognizerState.Began){
-            isRecording = true
-            captureAnimationBar()
-            print ("start recording")
-            fireTorch(sender)
-            //let outputPath = NSTemporaryDirectory() + "output.mov"
-            //let outputFileURL = NSURL(fileURLWithPath: outputPath)
-            //videoFileOutput?.startRecordingToOutputFileURL(outputFileURL, recordingDelegate: self)
-            }}
     
     func captureAnimationBar() {
         let coloredSquare = UIView()
@@ -253,7 +226,7 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
         
     }
     
-    // MARK: FLASH METHODS
+// MARK: FLASH METHODS
     
     func fireTorch(sender: AnyObject) {
         print("Called", currentDevice!)
@@ -289,6 +262,58 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
         }
     }
 
+// MARK: RELEGATED CAMERA METHODS
+//    func loadCamera(){
+//    //set camera to highest resolution device will support
+//    captureSession.sessionPreset = AVCaptureSessionPresetHigh
+//    // create array of available devices (front camera, back camera, microphone)
+//    let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo) as! [AVCaptureDevice]
+//        
+//    for device in devices {
+//        if device.position == AVCaptureDevicePosition.Back {
+//            backFacingCamera = device
+//        } else if device.position == AVCaptureDevicePosition.Front {
+//            frontFacingCamera = device
+//        }
+//    }
+//        
+//    currentDevice = backFacingCamera
+//    
+//    let captureDeviceInput:AVCaptureDeviceInput
+//        do {
+//            captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice)
+//        } catch {
+//            print(error)
+//            return
+//    }
+//    
+//    //create instance used to save data for movie file
+//    videoFileOutput = AVCaptureMovieFileOutput()
+//    videoFileOutput?.maxRecordedDuration
+//        
+//    //create instance used to save audio data
+//        
+//    let audioDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeAudio)
+//        
+//    let audioInput:AVCaptureDeviceInput
+//        do {
+//            audioInput = try AVCaptureDeviceInput(device:audioDevice)
+//        } catch {
+//            print(error)
+//            return
+//    }
+//    
+//    audioFileOutput = AVCaptureAudioDataOutput()
+//        
+//    //Assign the input and output devices to the capture session
+//    //captureSession.addInput(captureDeviceInput)
+//    captureSession.addInput(audioInput)
+//    captureSession.addOutput(videoFileOutput)
+//    captureSession.addOutput(audioFileOutput)
+//        
+//    }
+    
+    
     
     //MARK: FILE PROCESSING METHODS
 //    func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
@@ -310,37 +335,37 @@ class FlowMoController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
 //    }
     
     
-    func generateImageSequence(outputFileURL: NSURL) {
-        let avURLAsset = AVURLAsset(URL: outputFileURL, options:nil)
-        
-        
-        let imageGenerator = AVAssetImageGenerator.init(asset: avURLAsset)
-        imageGenerator.requestedTimeToleranceAfter=kCMTimeZero
-        imageGenerator.requestedTimeToleranceBefore=kCMTimeZero
-        
-        var imageHashRate: [NSValue] = []
-        //NEED TO PLUG THESE VALUES INTO THE BELOW LOOP TO GENERATE imageHashRateArray ONCE FURTHER WORK DONE
-        var loopDuration = avURLAsset.duration.value
-        let timeValue = Float(CMTimeGetSeconds(avURLAsset.duration))
-        
-        for var t = 0; t < 1800; t += 20 {
-            let cmTime = CMTimeMake(Int64(t), avURLAsset.duration.timescale)
-            let timeValue = NSValue(CMTime: cmTime)
-            imageHashRate.append(timeValue)
-            }
-        
-        var flowMoImageArray: [UIImage] = []
-        imageGenerator.generateCGImagesAsynchronouslyForTimes(imageHashRate) {(requestedTime, image, actualTime, result, error) -> Void in
-            if (result == .Succeeded) {
-                flowMoImageArray.append(UIImage(CGImage: image!))
-            }
-            if (result == .Failed) {
-                
-            }
-            if (result == .Cancelled) {
-                
-            }
-        }
-    }
-        
+//    func generateImageSequence(outputFileURL: NSURL) {
+//        let avURLAsset = AVURLAsset(URL: outputFileURL, options:nil)
+//        
+//        
+//        let imageGenerator = AVAssetImageGenerator.init(asset: avURLAsset)
+//        imageGenerator.requestedTimeToleranceAfter=kCMTimeZero
+//        imageGenerator.requestedTimeToleranceBefore=kCMTimeZero
+//        
+//        var imageHashRate: [NSValue] = []
+//        //NEED TO PLUG THESE VALUES INTO THE BELOW LOOP TO GENERATE imageHashRateArray ONCE FURTHER WORK DONE
+//        var loopDuration = avURLAsset.duration.value
+//        let timeValue = Float(CMTimeGetSeconds(avURLAsset.duration))
+//        
+//        for var t = 0; t < 1800; t += 20 {
+//            let cmTime = CMTimeMake(Int64(t), avURLAsset.duration.timescale)
+//            let timeValue = NSValue(CMTime: cmTime)
+//            imageHashRate.append(timeValue)
+//            }
+//        
+//        var flowMoImageArray: [UIImage] = []
+//        imageGenerator.generateCGImagesAsynchronouslyForTimes(imageHashRate) {(requestedTime, image, actualTime, result, error) -> Void in
+//            if (result == .Succeeded) {
+//                flowMoImageArray.append(UIImage(CGImage: image!))
+//            }
+//            if (result == .Failed) {
+//                
+//            }
+//            if (result == .Cancelled) {
+//                
+//            }
+//        }
+//    }
+    
 }
