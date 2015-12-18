3da8e892ce709d77627bb91ad82b311115e32af3
BryanRyczek <bryanryczek@gmail.com>
2015-12-18 09:39:04 -0500

    audio recorder and audio player updates

--- FlowMoAudioRecorder.swift
+++ FlowMoAudioRecorder.swift
@@ -1,102 +1,109 @@
-////
-////  FlowMoAudioRecorder.swift
-////  FlowMoSwift
-////
-////  Created by Bryan Ryczek on 12/16/15.
-////  Copyright © 2015 Bryan Ryczek. All rights reserved.
-////
 //
-//import UIKit
-//import AVFoundation
-//import Foundation
+//  FlowMoAudioRecorder.swift
+//  FlowMoSwift
 //
-//class FlowMoAudioRecorder: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+//  Created by Bryan Ryczek on 12/16/15.
+//  Copyright © 2015 Bryan Ryczek. All rights reserved.
 //
-//    var audioRecorder: AVAudioRecorder!
-//    var recordingSession: AVAudioSession!
-//    var audioPlayer: AVAudioPlayer
-//    var audioURL: NSURL
-//    
-//    
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        recorderSetup()
-//    }
-//    
-//    func recorderSetup() {
-//        recordingSession = AVAudioSession.sharedInstance()
-//        
-//        do {
-//            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
-//            try recordingSession.setActive(true)
-//            recordingSession.requestRecordPermission() { [unowned self] (allowed: Bool) -> Void in
-//                dispatch_async(dispatch_get_main_queue()) {
-//                    if allowed {
-//                        //Do nothing
-//                    } else {
-//                        // failed to record!
-//                    }
-//                }
-//            }
-//        } catch {
-//            // failed to record!
-//        }
-//    }
-//    
-//    func loadRecordingUI() {
-//        //RECORDING UI
-//    }
-//    
-//    func startRecording() {
-//        let audioFilename = getDocumentsDirectory().URLByAppendingPathComponent("recording.m4a")
-//        let audioURL = NSURL(fileURLWithPath: audioFilename)
-//        
-//        let settings = [
-//            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-//            AVSampleRateKey: 12000.0,
-//            AVNumberOfChannelsKey: 1 as NSNumber,
-//            AVEncoderAudioQualityKey: AVAudioQuality.High.rawValue
-//        ]
-//        
-//        do {
-//            audioRecorder = try AVAudioRecorder(URL: audioURL, settings: settings)
-//            audioRecorder.delegate = self
-//            audioRecorder.record()
-//        } catch {
-//            finishRecording(success: false)
-//        }
-//    }
-//    
-//    func getDocumentsDirectory() -> String {
-//        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
-//        let documentsDirectory = paths[0]
-//        return documentsDirectory
-//    }
-//    
-//    func finishRecording(success success:Bool) {
-//        audioRecorder.stop()
-//        audioRecorder = nil
-//        if success {
-//            // Recording Successful!
-//        } else {
-//            // Recording Failed!
-//        }
-//    }
-//    
-//    func flowmoRecording() {
-//        if audioRecorder == nil {
-//            startRecording()
-//        } else {
-//            finishRecording(success: true)
-//        }
-//    }
-//    
-//    // If iOS stops recording, this will handle the interruption so we aren't still recording
-//    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
-//        if !flag {
-//            finishRecording(success: false)
-//        }
-//    }
-//    
-//}
+
+
+import UIKit
+import AVFoundation
+import Foundation
+
+class FlowMoAudioRecorder: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+    //MARK: GLOBAL VARS
+    var recordButton: UIButton!
+    var audioRecorder: AVAudioRecorder!
+    var recordingSession: AVAudioSession!
+    
+    // MARK: SETUP METHODS
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        recorderSetup()
+    }
+    
+    func recorderSetup() {
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] (allowed: Bool) -> Void in
+                dispatch_async(dispatch_get_main_queue()) {
+                    if allowed {
+                        self.loadRecordingUI()
+                    } else {
+                        // failed to record!
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
+    }
+    // MARK: UI METHODS
+    func loadRecordingUI() {
+        recordButton = UIButton(frame: CGRect(x: 64, y: 64, width: 128, height: 64))
+        recordButton.backgroundColor = UIColor.blackColor()
+        recordButton.setTitle("Tap to Record", forState: .Normal)
+        recordButton.titleLabel?.font = UIFont.preferredFontForTextStyle(UIFontTextStyleTitle1)
+        recordButton.addTarget(self, action: "recordTapped", forControlEvents: .TouchUpInside)
+        view.addSubview(recordButton)
+    }
+    // MARK: RECORDING METHODS
+    func recordTapped() {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }    
+
+    
+    func startRecording() {
+        let audioFilename = try! NSFileManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("recording.m4a").path!
+        //let audioFilename = getDocumentsDirectory().URLByAppendingPathComponent("recording.m4a")
+        let audioURL = NSURL(fileURLWithPath: audioFilename)
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000.0,
+            AVNumberOfChannelsKey: 1 as NSNumber,
+            AVEncoderAudioQualityKey: AVAudioQuality.High.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(URL: audioURL, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success success:Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        if success {
+            print("Recording Successful")
+        } else {
+            print("Recording Failed")
+        }
+    }
+    
+    //MARK: DATA HANDLING METHODS
+    func getDocumentsDirectory() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    // MARK: HELPER METHODS
+    // If iOS stops recording, this will handle the interruption so we aren't still recording
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+    
+}
---
+++ FlowMoSwift/FlowMoAudioPlayer.swift
@@ -0,0 +1,39 @@
+//
+//  FlowMoAudioPlayer.swift
+//  FlowMoSwift
+//
+//  Created by Bryan Ryczek on 12/18/15.
+//  Copyright © 2015 Bryan Ryczek. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class FlowMoAudioPlayer: UIViewController {
+
+    var audioPlayer: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func playAudio() {
+        let path = NSBundle.mainBundle().pathForResource("sliceBombFuse.caf", ofType:nil)!
+        let url = NSURL(fileURLWithPath: path)
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOfURL: url)
+            audioPlayer = sound
+            sound.play()
+        } catch {
+            print("contents")
+        }
+    }
+    
+    func stopAudio() {
+        if audioPlayer != nil {
+            audioPlayer.stop()
+            audioPlayer = nil
+        }
+    }
+}
